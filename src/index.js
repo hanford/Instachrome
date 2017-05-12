bec8264134fecf4fae24467fe2dcccc11b4f1898
Jack Hanford <jackhanford@gmail.com>
2017-05-11 17:58:42 -0700

    correctly spoof ua 📱

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-const ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
-const vendor = 'Apple, Inc.'
-const platform = 'iPhone'
-
-window.navigator.__defineGetter__('userAgent', () => ua)
-window.navigator.__defineGetter__('appVersion', () => ua)
-window.navigator.__defineGetter__('vendor', () => vendor)
-window.navigator.__defineGetter__('platform', () => platform)
-
-
 function setUserAgent (window, callback) {
   var userAgentProp = {
     get: function () { return ua },
@@ -30,9 +20,10 @@ function setUserAgent (window, callback) {
 
 let frame = document.createElement('iframe')
 
-frame.src = 'https://www.instagram.com'
-
 document.body.appendChild(frame)
 
-setUserAgent(frame.contentWindow, () => {
+const frameWindow = frame.contentWindow
+
+setUserAgent(frameWindow, () => {
+  frame.src = 'https://www.instagram.com/?mobile=true'
 })
---
+++ src/ua-override.js
@@ -0,0 +1,21 @@
+const ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
+const vendor = 'Apple, Inc.'
+const platform = 'iPhone'
+
+let a = document.createElement('script')
+
+a.type = 'text/javascript'
+
+a.innerText += `
+  Object.defineProperty(window.navigator, 'userAgent', { get: () => { return '${ua}'; } });
+  Object.defineProperty(window.navigator, 'vendor', { get: () => { return '${vendor}'; } });
+  Object.defineProperty(window.navigator, 'platform', { get: () => { return '${platform}'; } });
+`
+
+document.documentElement.insertBefore(a, document.documentElement.firstChild)
+
+document.addEventListener('beforeload', e => {
+  Object.defineProperty(window.navigator, 'userAgent', { get: () => { return ua } })
+  Object.defineProperty(window.navigator, 'vendor', { get: () => { return vendor } })
+  Object.defineProperty(window.navigator, 'platform', { get: () => { return platform } })
+}, true)
